09aa8c90ef7049f3d6d2d943d7a10056a1487d99
weihanli <ben121011@126.com>
2016-11-01 01:39:18 +0800

    移除不必要的依赖项，分页模型增加是否显示跳转按钮

--- MvcSimplePager/IPagerModel.cs
+++ MvcSimplePager/IPagerModel.cs
@@ -12,48 +12,67 @@ namespace MvcSimplePager
         /// 分页显示模式
         /// </summary>
         PagingDisplayMode PagingDisplayMode { get; set; }
+
         /// <summary>
         /// 页码索引
         /// </summary>
         int PageIndex { get; set; }
+
         /// <summary>
         /// 页码容量，每页数据量
         /// </summary>
         int PageSize { get; set; }
+
         /// <summary>
         /// 数据总页数
         /// </summary>
         int PageCount { get; set; }
+
         /// <summary>
         /// 数据总数量
         /// </summary>
         int TotalCount { get; set; }
+
         /// <summary>
         /// 本页第一个元素索引
         /// </summary>
         int FirstItem { get; }
+
         /// <summary>
         /// 本页最后一个元素索引
         /// </summary>
         int LastItem { get; }
+
         /// <summary>
         /// 是否是第一页
         /// </summary>
         bool IsFirstPage { get; }
+
         /// <summary>
         /// 是否是最后一页
         /// </summary>
         bool IsLastPage { get; }
+
         /// <summary>
         /// 是否有上一页
         /// </summary>
         bool HasPreviousPage { get; }
+
         /// <summary>
         /// 是否有下一页
         /// </summary>
         bool HasNextPage { get; }
 
+        /// <summary>
+        /// 每组数据量
+        /// </summary>
         int GroupSize { get; set; }
+
+        /// <summary>
+        /// 是否显示跳转按钮
+        /// </summary>
+        bool ShowJumpButton { get; set; }
+
         /// <summary>
         /// 翻页路径或翻页处理事件
         /// </summary>
@@ -69,6 +88,7 @@ namespace MvcSimplePager
         /// always show the pager
         /// </summary>
         Always = 0,
+
         /// <summary>
         /// show pager only when pageSize > 1
         /// </summary>
--- MvcSimplePager/PagerModel.cs
+++ MvcSimplePager/PagerModel.cs
@@ -63,5 +63,7 @@ namespace MvcSimplePager
                 }
             }
         }
+
+        public bool ShowJumpButton { get; set; }
     }
 }
