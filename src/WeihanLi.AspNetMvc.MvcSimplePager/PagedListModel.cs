63e973d567f19f3773eb47ca9df99b28f9094fe6
WeihanLi <weihanli@outlook.com>
2019-08-20 14:50:01 +0800

    strongName and integrate with sourceLink

--- src/WeihanLi.AspNetMvc.MvcSimplePager/PagedListModel.cs
+++ src/WeihanLi.AspNetMvc.MvcSimplePager/PagedListModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("WeihanLi.AspNetMvc.MvcSimplePager.Test")]
+[assembly: InternalsVisibleTo(@"WeihanLi.AspNetMvc.MvcSimplePager.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b9567ca1495d374ebde24d16e7d47de27ae6c3ded022658f4e9b9dbe0858f1ed9383a4b50360229f2f75d650dc16350e097290c814fb77f5bf3f3dec54a6c6149e815f961a4732ef898e0ff20e15078c2ce64db505922f4766c37014b828123811d7f5cdfabd62fa00a7c3267ef2f1b456d8bf048e2307a51ff43a242249c4bd")]
 
 namespace WeihanLi.AspNetMvc.MvcSimplePager
 {
